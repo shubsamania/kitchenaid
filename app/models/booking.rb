4b6c613e420107f5a35c71f5dc603b1342d3a0ec
emmapreysing <emma@preysingmail.com>
2023-11-21 15:31:52 +0100

    Add views for ckitchens & bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,4 @@
 class Booking < ApplicationRecord
-  belongs_to :kitchen
+  belongs_to :kitchen, :user
   validates :total_price, :date, presence: true
-  validates :rating, inclusion: { in: 0..5, allow_nil: false }
-  validates :rating, numericality: { only_integer: true }
 end
