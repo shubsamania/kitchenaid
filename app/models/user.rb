8c53811e0e390260acc6a8c40628a85b1fe898e2
emmapreysing <emma@preysingmail.com>
2023-11-21 16:43:43 +0100

    Add missing relations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  # has_many :kitchens
-  # has_many :kitchens, through: :bookings
+  # Owner
+  has_many :bookings_as_owner, through: :kitchens, source: :bookings
+
+  # User
+  has_many :bookings
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/migrate/20231121131945_create_bookings.rb
+++ db/migrate/20231121131945_create_bookings.rb
@@ -1,9 +1,11 @@
 class CreateBookings < ActiveRecord::Migration[7.1]
   def change
     create_table :bookings do |t|
-      t.date :date
+      t.date :start_date
+      t.date :end_date
       t.integer :total_price
-
+      t.references :user, null: false, foreign_key: true
+      
       t.timestamps
     end
   end
