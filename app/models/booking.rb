23c8e34e7ad5c786a7dceeb6ead87f86678c9d8f
Shubham Gautam <gautam.shubham@gmail.com>
2023-11-23 15:22:44 +0100

    Cloudinary

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :kitchen
   belongs_to :user
+  has_one :review, dependent: :destroy
   # owner
   # belongs_to :owner, through: :kitchens, source: :users
   # validates :total_price, presence: true
--- app/models/kitchen.rb
+++ app/models/kitchen.rb
@@ -2,4 +2,5 @@ class Kitchen < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   validates :name, :price, :description, presence: true
+  has_one_attached :photo
 end
