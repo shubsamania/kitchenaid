f8c0a2a8778a618d6cb9eb3ee5d3400d3751dedf
emmapreysing <emma@preysingmail.com>
2023-11-24 12:28:38 +0100

    Edit navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
 end
